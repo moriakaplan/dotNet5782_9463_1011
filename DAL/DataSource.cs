dcdfe8986cd8b612381adc1a95cc7ebf9b295759
moriakaplan <mokaplan@g.jct.ac.il>
2021-10-18 13:14:56 +0300

    add 2 drones

--- DAL/DataSource.cs
+++ DAL/DataSource.cs
@@ -6,10 +6,33 @@ namespace DalObject
 {
     class DataSource
     {
-        internal List<Drone> drones = new List<Drone>();
-        internal List<DroneCharge> droneCharges = new List<DroneCharge>();
-        internal List<Parcel> parcels = new List<Parcel>();
-        internal List<Customer> customers = new List<Customer>();
-        internal List<Station> stations = new List<Station>();
+        internal static List<Drone> drones = new List<Drone>();
+        internal static List<DroneCharge> droneCharges = new List<DroneCharge>();
+        internal static List<Parcel> parcels = new List<Parcel>();
+        internal static List<Customer> customers = new List<Customer>();
+        internal static List<Station> stations = new List<Station>();
+        internal static class Config
+        {
+            internal static int parcelCode = 0;
+            internal static Random random = new Random();
+        }
+        static void Initialize()
+        {
+            drones.Add(new Drone { 
+                Id = 3456,// for parcel use: Config.parcelCode++, 
+                Battery = 98.99,
+                MaxWeight = WeightCategories.easy, 
+                Model = "superFalcon",
+                Status = DroneStatuses.vacant 
+            });
+            drones.Add(new Drone { 
+                Id = 6778, 
+                Battery = 98.99,
+                MaxWeight = WeightCategories.medium, 
+                Model = "AnaAref",
+                Status = DroneStatuses.vacant 
+            });
+        }
+
     }
 }
