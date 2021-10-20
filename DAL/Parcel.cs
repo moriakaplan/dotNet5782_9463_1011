df3b7021b6f7170d488afb84697a4ddc76b87889
moriakaplan <mokaplan@g.jct.ac.il>
2021-10-20 15:23:12 +0300

    names in parcel

--- DAL/Parcel.cs
+++ DAL/Parcel.cs
@@ -9,13 +9,13 @@ namespace IDAL
             public int Id { get; set; }
             public int Senderld { get; set; }
             public int TargetId { get; set; }
-            public WeightCategories Weight { get; set; }
-            public Priorities Priority { get; set; }
-            public DateTime Requested { get; set; }
             public int Droneld { get; set; }
-            public DateTime Scheduled { get; set; }
-            public DateTime PickedUp { get; set; }
-            public DateTime Delivered { get; set; }
+            public WeightCategories Weight { get; set; }
+            public Priorities Priority { get; set; } //רגיל, מהיר, חירום
+            public DateTime Requested { get; set; } //יצירת החבילה
+            public DateTime Scheduled { get; set; } //שיוך לרחפן
+            public DateTime PickedUp { get; set; }  //איסוף מהשולח
+            public DateTime Delivered { get; set; } //מסירה ליעד
             public override string ToString()
             {
                 return $"parcel #{Id}: sender ID- {Senderld}, target ID- {TargetId}, weight- {Weight}, priority- {Priority}, requested- {Requested} ,droneld- {Droneld}, scheduled- {Scheduled}, picked up- {PickedUp}, delivered- {Delivered}";
