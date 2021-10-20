f744af1b602c3a9b45f02594c88c26bc8c86ba90
shuvadanziger <shuva@g.jct.ac.il>
2021-10-20 14:38:55 +0300

    :)

--- DAL/DataSource.cs
+++ DAL/DataSource.cs
@@ -11,146 +11,29 @@ namespace DalObject
         internal static List<Parcel> parcels = new List<Parcel>();
         internal static List<Customer> customers = new List<Customer>();
         internal static List<Station> stations = new List<Station>();
-        internal static Random random = new Random();
         internal static class Config
         {
             internal static int parcelCode = 0;
+            internal static Random random = new Random();
         }
         static void Initialize()
         {
-            stations.Add(new Station
-            {
-                Id = random.Next(1000, 10000),
-                Name = "the israelian station",
-                Longitude = 34.8,
-                Lattitude = 32,
-                ChargeSlots = 163
-            }) ;
-            stations.Add(new Station
-            {
-                Id = random.Next(1000, 10000),
-                Name = "the biggest station",
-                Longitude = -45,
-                Lattitude = 78.3,
-                ChargeSlots = 5408
-            });
             drones.Add(new Drone { 
-                Id = random.Next(100000, 1000000),// for parcel use: Config.parcelCode++, 
-                Battery = random.Next(100),
+                Id = 3456,// for parcel use: Config.parcelCode++, 
+                Battery = 98.99,
                 MaxWeight = WeightCategories.Easy, 
                 Model = "superFalcon",
                 Status = DroneStatuses.Vacant 
             });
             drones.Add(new Drone { 
-                Id = random.Next(100000, 1000000), 
-                Battery = random.Next(100),
+                Id = 6778, 
+                Battery = 98.99,
                 MaxWeight = WeightCategories.Medium, 
                 Model = "AnaAref",
-                Status = DroneStatuses.Maintenance 
-            });
-            drones.Add(new Drone {
-                Id = random.Next(100000, 1000000),// for parcel use: Config.parcelCode++, 
-                Battery = random.Next(100),
-                MaxWeight = WeightCategories.Easy,
-                Model = "superFalcon",
-                Status = DroneStatuses.Vacant
-            });
-            drones.Add(new Drone {
-                Id = random.Next(100000, 1000000),// for parcel use: Config.parcelCode++, 
-                Battery = random.Next(100),
-                MaxWeight = WeightCategories.Heavy,
-                Model = "superFalcon2",
-                Status = DroneStatuses.Sending
-            });
-            drones.Add(new Drone
-            {
-                Id = random.Next(100000, 1000000),// for parcel use: Config.parcelCode++, 
-                Battery = random.Next(100),
-                MaxWeight = WeightCategories.Easy,
-                Model = "nimbus2000",
-                Status = DroneStatuses.Vacant
-            });
-            customers.Add(new Customer
-            {
-                Id = random.Next(1000000000),
-                Name = "Yosef",
-                Phone = "0501234567",
-                Longitude = random.Next(-180, 180)/*35.2*/,
-                Lattitude = random.Next(-180, 180)/*31.1*/
-            });
-            customers.Add(new Customer
-            {
-                Id = random.Next(1000000000),
-                Name = "Avi",
-                Phone = "0503456789",
-                Longitude = random.Next(-180, 180),
-                Lattitude = random.Next(-180, 180)
-            });
-            customers.Add(new Customer
-            {
-                Id = random.Next(1000000000),
-                Name = "Nahum",
-                Phone = "0545678901",
-                Longitude = random.Next(-180, 180),
-                Lattitude = random.Next(-180, 180)
-            }); 
-            customers.Add(new Customer
-            {
-                Id = random.Next(1000000000),
-                Name = "Moshe",
-                Phone = "0523456789",
-                Longitude = random.Next(-180, 180),
-                Lattitude = random.Next(-180, 180)
-            }); 
-            customers.Add(new Customer
-            {
-                Id = random.Next(1000000000),
-                Name = "Shlomo",
-                Phone = "0521234567",
-                Longitude = random.Next(-180, 180),
-                Lattitude = random.Next(-180, 180)
-            }); 
-            customers.Add(new Customer
-            {
-                Id = random.Next(1000000000),
-                Name = "Shira",
-                Phone = "0502345678",
-                Longitude = random.Next(-180, 180),
-                Lattitude = random.Next(-180, 180)
-            }); 
-            customers.Add(new Customer
-            {
-                Id = random.Next(1000000000),
-                Name = "Naama",
-                Phone = "0531234567",
-                Longitude = random.Next(-180, 180),
-                Lattitude = random.Next(-180, 180)
-            }); 
-            customers.Add(new Customer
-            {
-                Id = random.Next(1000000000),
-                Name = "Etya",
-                Phone = "0501234569",
-                Longitude = random.Next(-180, 180),
-                Lattitude = random.Next(-180, 180)
-            }); 
-            customers.Add(new Customer
-            {
-                Id = random.Next(1000000000),
-                Name = "Yosefa",
-                Phone = "0501237567",
-                Longitude = random.Next(-180, 180),
-                Lattitude = random.Next(-180, 180)
-            }); 
-            customers.Add(new Customer
-            {
-                Id = random.Next(1000000000),
-                Name = "Yosi",
-                Phone = "0541234567",
-                Longitude = random.Next(-180, 180),
-                Lattitude = random.Next(-180, 180)
+                Status = DroneStatuses.Sending 
             });
 
         }
+
     }
 }
