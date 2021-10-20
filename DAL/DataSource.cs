dec91e4272fa879b6801c4b0d8889edd55615886
moriakaplan <mokaplan@g.jct.ac.il>
2021-10-20 14:18:16 +0300

    initialize- start

--- DAL/DataSource.cs
+++ DAL/DataSource.cs
@@ -11,26 +11,137 @@ namespace DalObject
         internal static List<Parcel> parcels = new List<Parcel>();
         internal static List<Customer> customers = new List<Customer>();
         internal static List<Station> stations = new List<Station>();
+        internal static Random random = new Random();
         internal static class Config
         {
             internal static int parcelCode = 0;
-            internal static Random random = new Random();
         }
         static void Initialize()
         {
+            stations.Add(new Station
+            {
+                Id = random.Next(1000, 10000),
+                Name = "the israelian station",
+                Longitude = 34.8,
+                Lattitude = 32,
+                ChargeSlots = 163
+            }) ;
+            stations.Add(new Station
+            {
+                Id = random.Next(1000, 10000),
+                Name = "the biggest station",
+                Longitude = -45,
+                Lattitude = 78.3,
+                ChargeSlots = 5408
+            });
             drones.Add(new Drone { 
-                Id = 3456,// for parcel use: Config.parcelCode++, 
-                Battery = 98.99,
+                Id = random.Next(100000, 1000000),// for parcel use: Config.parcelCode++, 
+                Battery = random.Next(100),
                 MaxWeight = WeightCategories.Easy, 
                 Model = "superFalcon",
                 Status = DroneStatuses.Vacant 
             });
             drones.Add(new Drone { 
-                Id = 6778, 
-                Battery = 98.99,
+                Id = random.Next(100000, 1000000), 
+                Battery = random.Next(100),
                 MaxWeight = WeightCategories.Medium, 
                 Model = "AnaAref",
-                Status = DroneStatuses.Sending 
+                Status = DroneStatuses.Maintenance 
+            });
+            drones.Add(new Drone {
+                Id = random.Next(100000, 1000000),// for parcel use: Config.parcelCode++, 
+                Battery = random.Next(100),
+                MaxWeight = WeightCategories.Easy,
+                Model = "superFalcon",
+                Status = DroneStatuses.Vacant
+            });
+            drones.Add(new Drone {
+                Id = random.Next(100000, 1000000),// for parcel use: Config.parcelCode++, 
+                Battery = random.Next(100),
+                MaxWeight = WeightCategories.Heavy,
+                Model = "superFalcon2",
+                Status = DroneStatuses.Sending
+            });
+            drones.Add(new Drone
+            {
+                Id = random.Next(100000, 1000000),// for parcel use: Config.parcelCode++, 
+                Battery = random.Next(100),
+                MaxWeight = WeightCategories.Easy,
+                Model = "nimbus2000",
+                Status = DroneStatuses.Vacant
+            });
+            customers.Add(new Customer
+            {
+                Id = random.Next(1000000000),
+                Name = "Yosef",
+                Phone = "0501234567",
+                Longitude = random.Next(-180, 180)/*35.2*/,
+                Lattitude = random.Next(-180, 180)/*31.1*/
+            });
+            customers.Add(new Customer
+            {
+                Id = random.Next(1000000000),
+                Name = "Yosef",
+                Phone = "0501234567",
+                Longitude = random.Next(-180, 180),
+                Lattitude = random.Next(-180, 180)
+            });
+            customers.Add(new Customer
+            {
+                Id = random.Next(1000000000),
+                Name = "Yosef",
+                Phone = "0501234567",
+                Longitude = random.Next(-180, 180),
+                Lattitude = random.Next(-180, 180)
+            }); customers.Add(new Customer
+            {
+                Id = random.Next(1000000000),
+                Name = "Yosef",
+                Phone = "0501234567",
+                Longitude = random.Next(-180, 180),
+                Lattitude = random.Next(-180, 180)
+            }); customers.Add(new Customer
+            {
+                Id = random.Next(1000000000),
+                Name = "Yosef",
+                Phone = "0501234567",
+                Longitude = random.Next(-180, 180),
+                Lattitude = random.Next(-180, 180)
+            }); customers.Add(new Customer
+            {
+                Id = random.Next(1000000000),
+                Name = "Yosef",
+                Phone = "0501234567",
+                Longitude = random.Next(-180, 180),
+                Lattitude = random.Next(-180, 180)
+            }); customers.Add(new Customer
+            {
+                Id = random.Next(1000000000),
+                Name = "Yosef",
+                Phone = "0501234567",
+                Longitude = random.Next(-180, 180),
+                Lattitude = random.Next(-180, 180)
+            }); customers.Add(new Customer
+            {
+                Id = random.Next(1000000000),
+                Name = "Yosef",
+                Phone = "0501234567",
+                Longitude = random.Next(-180, 180),
+                Lattitude = random.Next(-180, 180)
+            }); customers.Add(new Customer
+            {
+                Id = random.Next(1000000000),
+                Name = "Yosef",
+                Phone = "0501234567",
+                Longitude = random.Next(-180, 180),
+                Lattitude = random.Next(-180, 180)
+            }); customers.Add(new Customer
+            {
+                Id = random.Next(1000000000),
+                Name = "Yosef",
+                Phone = "0501234567",
+                Longitude = random.Next(-180, 180),
+                Lattitude = random.Next(-180, 180)
             });
         }
 
--- DAL/Station.cs
+++ DAL/Station.cs
@@ -7,7 +7,7 @@ namespace IDAL
         public struct Station
         {
             public int Id { get; set; }
-            public int Name { get; set; }
+            public string Name { get; set; }
             public Double Longitude { get; set; }
             public Double Lattitude { get; set; }
             public int ChargeSlots { get; set; }
