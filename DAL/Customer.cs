2eb26b4a0147134710541ce6c4e31e31e8b43f49
moriakaplan <mokaplan@g.jct.ac.il>
2021-10-31 17:47:10 +0200

    ...

--- DAL/Customer.cs
+++ DAL/Customer.cs
@@ -12,31 +12,34 @@ namespace IDAL
             public int Id { get; set; }
             public string Name { get; set; }
             public string Phone { get; set; }
-            private Coordinate longi;
-            private Coordinate latti;
-            
-            public double Longitude//bonus 1
-            {
-                get { return longi.Value; }
-                set
-                {
-                    longi = new Coordinate();
-                    longi.Value = value;
-                    longi.IsLongitude = true;
-                }
-            }
-            public double Lattitude//bonus 1
-            {
-                get { return latti.Value; }
-                set
-                {
-                    latti = new Coordinate();
-                    latti.Value = value;
-                    latti.IsLongitude = false;
-                }
-            }
+            public double Longitude { get; set; }
+            public double Lattitude { get; set; }
+            //private Coordinate longi;
+            //private Coordinate latti;
+            //public double Longitude//bonus 1
+            //{
+            //    get { return longi.Value; }
+            //    set
+            //    {
+            //        longi = new Coordinate();
+            //        longi.Value = value;
+            //        longi.IsLongitude = true;
+            //    }
+            //}
+            //public double Lattitude//bonus 1
+            //{
+            //    get { return latti.Value; }
+            //    set
+            //    {
+            //        latti = new Coordinate();
+            //        latti.Value = value;
+            //        latti.IsLongitude = false;
+            //    }
+            //}
             public override string ToString()
             {
+                Coordinate longi = new Coordinate { Value = Longitude, IsLongitude = true };
+                Coordinate latti = new Coordinate { Value = Longitude, IsLongitude = false };
                 return @$"customer #{Id}:
 number- {Name},
 phone- {Phone},
--- DAL/Station.cs
+++ DAL/Station.cs
@@ -11,27 +11,34 @@ namespace IDAL
         {
             public int Id { get; set; }
             public string Name { get; set; }
-            private Coordinate longi;
-            private Coordinate latti;
-            public double Longitude//bonus 1
-            {
-                get { return longi.Value; }
-                set { longi = new Coordinate(value, true); }
-            }
-            public double Lattitude//bonus 1
-            {
-                get { return latti.Value; }
-                set
-                {
-                    latti = new Coordinate();
-                    latti.Value = value;
-                    latti.IsLongitude = false;
-                }
-            }
+            public double Longitude { get; set; }
+            public double Lattitude { get; set; }
+            //private Coordinate longi;
+            //private Coordinate latti;
+            //public double Longitude//bonus 1
+            //{
+            //    get { return longi.Value; }
+            //    set { 
+            //        longi = new Coordinate();
+            //        longi.Value = value;
+            //        longi.IsLongitude = false;
+            //    }
+            //}
+            //public double Lattitude//bonus 1
+            //{
+            //    get { return latti.Value; }
+            //    set
+            //    {
+            //        latti = new Coordinate();
+            //        latti.Value = value;
+            //        latti.IsLongitude = false;
+            //    }
+            //}
             public int ChargeSlots { get; set; }
             public override string ToString()
             {
-
+                Coordinate longi = new Coordinate { Value = Longitude, IsLongitude = true };
+                Coordinate latti = new Coordinate { Value = Longitude, IsLongitude = false };
                 return @$"station #{Id}:
 number- {Name}, 
 longitude- {longi},
