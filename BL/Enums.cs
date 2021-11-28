3a75d1c054fd82d8f29bd8cb1053c503181b83d1
moriakaplan <mokaplan@g.jct.ac.il>
2021-11-28 21:57:45 +0200

    changes

--- BL/Enums.cs
+++ BL/Enums.cs
@@ -8,6 +8,6 @@ namespace IBL.BO
 {
     public enum WeightCategories { Easy, Medium, Heavy };
     public enum Priorities { Regular, Fast, Emergency };
-    public enum ParcelStatus { Created, Associated, PicedUp, Delivered };
+    public enum ParcelStatus { Created, Associated, PickedUp, Delivered };
     public enum DroneStatus { Available, Associated, Maintenance, Delivery };
 }
