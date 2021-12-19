2aa43516cec11825c61ddce70a41e3d8d8c8abca
shuvadanziger <shuva@g.jct.ac.il>
2021-12-19 16:47:54 +0200

    start 1

---
+++ DAL/DO/Drone.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace DO
+{
+    /// <summary>
+    ///רחפנים 
+    /// </summary>
+    public struct Drone
+    {
+        public int Id { get; set; }
+        public string Model { get; set; }
+        public WeightCategories MaxWeight { get; set; }
+
+        public override string ToString()
+        {
+            return @$"drone #{Id}:
+model- {Model},
+max weight- {MaxWeight}
+";
+            //                status - { Status},
+            //battery - { Battery}
+        }
+    }
+}
+
+
--- DAL/IDal.cs
+++ DAL/IDal.cs
@@ -1,8 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
-using IDAL.DO;
+using DO;
 
-namespace IDAL
+namespace DalApi
 {
     public interface IDal
     {
