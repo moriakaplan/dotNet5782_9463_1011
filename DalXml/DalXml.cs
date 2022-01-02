da1a9b0d38076dc49e2269a7c8a4aa5307952972
shuvadanziger <shuva@g.jct.ac.il>
2022-01-02 18:26:38 +0200

    .

---
+++ DalXml/DalXml.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using DalApi;
+
+namespace Dal
+{
+    public class DalXml: IDal
+    {
+        #region singleton
+        private static DalXml instance;
+        private static object syncRoot = new object();
+        /// <summary>
+        /// constructor.call the static function initialize.
+        /// </summary>
+        private DalXml()
+        {
+            //DataSource.Initialize();
+        }
+        /// <summary>
+        /// The public Instance property to use
+        /// </summary>
+        internal static DalXml Instance
+        {
+            //singelton thread safe and lazy initializion(?)
+            get
+            {
+                if (instance == null)
+                {
+                    lock (syncRoot)
+                    {
+                        if (instance == null)
+                            instance = new DalXml();
+                    }
+                }
+                return instance;
+            }
+        }
+        #endregion
+
+
+    }
+}
