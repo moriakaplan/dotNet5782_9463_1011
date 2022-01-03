bc2ae094b2a6466d44024bae88be671dccf5ea71
shuvadanziger <shuva@g.jct.ac.il>
2022-01-03 11:08:20 +0200

    DalXmlDrone

--- DalXml/DalXmlDrone.cs
+++ DalXml/DalXmlDrone.cs
@@ -20,50 +20,62 @@ namespace Dal
         }
         public void SendDroneToCharge(int droneId, int stationId)
         {
-            //if (!DataSource.drones.Exists(item => item.Id == droneId))
-            //{
-            //    throw new DroneException($"id: {droneId} does not exist");
-            //}
-            //DroneCharge droneCharge = new DroneCharge { DroneId = droneId, StationId = stationId, StartedChargeTime = DateTime.Now }; //add drone charge to the list for charging the drone
-            //DataSource.droneCharges.Add(droneCharge);
-            //for (int i = 0; i < DataSource.stations.Count; i++) //find the station and update its details
-            //{
-            //    if (DataSource.stations[i].Id == stationId)
-            //    {
-            //        if (DataSource.stations[i].ChargeSlots < 1) throw new StationException("");
-            //        Station s = DataSource.stations[i];
-            //        s.ChargeSlots--; //++?
-            //        DataSource.stations[i] = s;
-            //        return;
-            //    }
-            //}
-            //throw new StationException($"id: {stationId} does not exist");
+            
+            List<Drone> drones = XmlTools.LoadListFromXmlSerializer<Drone>(dronesPath);
+            if (drones.Exists(item => item.Id == droneId)) throw new DroneException($"id: {droneId} already exist");
+            DroneCharge droneCharge = new DroneCharge { DroneId = droneId, StationId = stationId, StartedChargeTime = DateTime.Now }; //add drone charge to the list for charging the drone                                                                                                                          //DataSource.droneCharges.Add(droneCharge);
+            List<DroneCharge> dCharge = XmlTools.LoadListFromXmlSerializer<DroneCharge>(droneChargesPath);
+            dCharge.Add(droneCharge);
+            List<Station> stations = XmlTools.LoadListFromXmlSerializer<Station>(stationsPath);
+            for (int i = 0; i < stations.Count; i++) //find the station and update its details
+            {
+                if (stations[i].Id == stationId)
+               {
+                   if (stations[i].ChargeSlots < 1) throw new StationException("");
+                    Station s = stations[i];
+                    s.ChargeSlots--; //++?
+                    stations[i] = s;
+                    return;
+                }
+            }
+            throw new StationException($"id: {stationId} does not exist");
+            XmlTools.SaveListToXmlSerializer<Drone>(drones, dronesPath);
+            XmlTools.SaveListToXmlSerializer<DroneCharge>(dCharge, droneChargesPath);
+            XmlTools.SaveListToXmlSerializer<Station>(stations, stationsPath);
+
+
         }
         public void ReleaseDroneFromeCharge(int droneId)
         {
-            //DroneCharge dCharge;
-            //if (!DataSource.droneCharges.Exists(x => x.DroneId == droneId))
-            //    throw new DroneChargeException($"drone charge with the drone ID {droneId} does not exist");
-            //dCharge = DataSource.droneCharges.Find(x => x.DroneId == droneId);
-            //DataSource.droneCharges.Remove(dCharge);
-            //for (int i = 0; i < DataSource.stations.Count; i++) //find the station and update its details
-            //{
-            //    if (DataSource.stations[i].Id == dCharge.StationId)
-            //    {
-            //        Station s = DataSource.stations[i];
-            //        s.ChargeSlots++; //--?
-            //        DataSource.stations[i] = s;
-            //        return;
-            //    }
-            //}
-            //throw new StationException($"id: {dCharge.StationId} does not exist");
+            DroneCharge dCharge;
+            List<DroneCharge> droneCharges = XmlTools.LoadListFromXmlSerializer<DroneCharge>(droneChargesPath);
+            if (!droneCharges.Exists(x => x.DroneId == droneId))
+                throw new DroneChargeException($"drone charge with the drone ID {droneId} does not exist");
+            dCharge = droneCharges.Find(x => x.DroneId == droneId);
+            droneCharges.Remove(dCharge);
+            List<Station> stations = XmlTools.LoadListFromXmlSerializer<Station>(stationsPath);
+
+            for (int i = 0; i < stations.Count; i++) //find the station and update its details
+            {
+                if (stations[i].Id == dCharge.StationId)
+                {
+                    Station s = stations[i];
+                    s.ChargeSlots++; //--?
+                    stations[i] = s;
+                    return;
+                }
+            }
+            throw new StationException($"id: {dCharge.StationId} does not exist");
+            XmlTools.SaveListToXmlSerializer<DroneCharge>(droneCharges, droneChargesPath);
+            XmlTools.SaveListToXmlSerializer<Station>(stations, stationsPath);
         }
         public Drone DisplayDrone(int droneId)
         {
-            //Drone? dr = DataSource.drones.Find(item => item.Id == droneId);
-            //if (dr == null)
-            //    throw new DroneException($"id: {droneId} does not exist");
-            //return (Drone)dr;
+            List<Drone> drones = XmlTools.LoadListFromXmlSerializer<Drone>(dronesPath);
+            Drone? dr =drones.Find(item => item.Id == droneId);
+            if (dr == null)
+                throw new DroneException($"id: {droneId} does not exist");
+            return (Drone)dr;
         }
         public IEnumerable<DroneCharge> DisplayListOfDroneCharge(Predicate<DroneCharge> pre)
         {
@@ -73,17 +85,20 @@ namespace Dal
         }
         public IEnumerable<Drone> DisplayListOfDrones(Predicate<Drone> pre)
         {
-            //List<Drone> result = new List<Drone>(DataSource.drones);
-            //if (pre == null) return result;
-            //return result.FindAll(pre);
+            List<Drone> drones = XmlTools.LoadListFromXmlSerializer<Drone>(dronesPath);
+            List<Drone> result = new List<Drone>(drones);
+            if (pre == null) return result;
+            return result.FindAll(pre);
         }
         public void DeleteDrone(int droneId)
         {
-            //try { DataSource.drones.Remove(DisplayDrone(droneId)); }
-            //catch (ArgumentNullException)
-            //{
-            //    throw new DroneException($"id: {droneId} does not exist");
-            //}
+            List<Drone> drones = XmlTools.LoadListFromXmlSerializer<Drone>(dronesPath);
+            try { drones.Remove(DisplayDrone(droneId)); }
+            catch (ArgumentNullException)
+            {
+                throw new DroneException($"id: {droneId} does not exist");
+            }
+            XmlTools.SaveListToXmlSerializer<Drone>(drones, dronesPath);
         }
     }
 }
